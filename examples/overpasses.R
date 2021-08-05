a1359fe63275b6e986be54eed8773ad52e9bfb4b
Zivan Karaman <zivan.karaman@limagrain.com>
2021-08-05 23:06:49 +0200

    added example scripts

---
+++ examples/overpasses.R
@@ -0,0 +1,38 @@
+# examples for overpasses over an area of interest
+# works only for Sentinel-2, Landsat-8 and Sentinel-1 satellites
+# this functionality requires an api_key, here we use default value Sys.getenv("spectator_earth_api_key")
+
+library(sf)
+
+# get the Luxembourg country shape as area of interest
+url <- "https://download.data.public.lu/resources/limites-administratives-du-grand-duche-de-luxembourg/20180806-153458/communes4326.geojson"
+boundary <- read_sf(url)
+
+# look for Sentinel-2 A and B, use shorthand notation, default time frame
+pass <- GetOverpasses(boundary, satellites = "S-2", acquisitions = TRUE)
+
+# do some nice graphs
+library(maps)
+days <- range(as.Date(pass$date))
+satellites <- sort(unique(pass$satellite))
+map(database = "world", region = c("Belgium", "Netherlands", "Germany", "Luxembourg", "France", "Switzerland"), col = "lightgrey", fill = TRUE)
+plot(sf::st_geometry(boundary), add = TRUE, col = "red", border = FALSE)
+plot(sf::st_geometry(pass), add = TRUE)
+title(main = sprintf("%s overpasses for period %s", paste(satellites, collapse = "/"), 
+                     paste(days, collapse = ":")))
+
+# look for Sentinel-1 A and B, use shorthand notation, last week  
+pass <- GetOverpasses(boundary, satellites = "S-1", days_before = 7, days_after = 0, acquisitions = TRUE)
+
+days_before = 0
+days_after = 7
+
+# do some nice graphs
+days <- range(as.Date(pass$date))
+satellites <- sort(unique(pass$satellite))
+map(database = "world", region = c("Belgium", "Netherlands", "Germany", "Luxembourg", "France", "Switzerland"), col = "lightgrey", fill = TRUE)
+plot(sf::st_geometry(boundary), add = TRUE, col = "red", border = FALSE)
+plot(sf::st_geometry(pass), add = TRUE)
+title(main = sprintf("%s overpasses for period %s", paste(satellites, collapse = "/"), 
+                     paste(days, collapse = ":")))
+
---
+++ examples/satellites.R
@@ -0,0 +1,32 @@
+# examples for using satellites related functions
+# works for all satellites referenced in the in the Spectator Earth database
+
+library(sf)
+
+# get all satellites withe their positions
+pos <- GetAllSatellites(positions = TRUE)
+
+# do some nice graphs
+library(maps)
+map("world", fill = TRUE, col = "lightgrey")
+# show open data satellites in green
+plot(st_geometry(subset(pos, open == TRUE)), add = TRUE, col = "green", pch = 15)
+# show others in red
+plot(st_geometry(subset(pos, open == FALSE)), add = TRUE, col = "red", pch = 16)
+# add labels
+xy <- st_coordinates(pos)
+# shift labels up to be able to read them
+xy[, 2] <- xy[, 2] + 2 
+text(xy, labels = pos$name, cex = 0.5)
+
+# get trajectory and current position for a selected satellite
+sat <- "SPOT-7"
+traj <- GetTrajectory(satellite = sat)
+pos <- GetSatellite(satellite = sat, positions = TRUE)
+
+# do some nice graphs
+map("world", fill = TRUE, col = "lightgrey")
+plot(st_geometry(traj), lwd = 2, col = "red", add = TRUE)
+plot(st_geometry(pos), pch = 15, col = "green", cex = 1.5, add = TRUE)
+title(main = sprintf("current %s trajectory & position", sat))
+
