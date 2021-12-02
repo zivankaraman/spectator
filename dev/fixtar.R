75568cc098d88026b2d96b44ef9b3601d7f5e5fe
Zivan Karaman <30553159+zivankaraman@users.noreply.github.com>
2021-12-02 20:06:31 +0100

    RC2

--- dev/fixtar.R
+++ dev/fixtar.R
@@ -1,6 +1,6 @@
 chemin <- gsub("\\\\", "/", tempfile("dir"))
-tgz <- "C:/GitHub/spectator_0.0.1.tar.gz"
-file.copy(from = tgz, to = "C:/GitHub/spectator_0.0.1.bkp.tar.gz", overwrite = TRUE)
+tgz <- "C:/GitHub/spectator_0.1.0.tar.gz"
+file.copy(from = tgz, to = "C:/GitHub/spectator_0.1.0.bkp.tar.gz", overwrite = TRUE)
 untar(tgz, exdir = chemin)
 dirto <- paste0(chemin, "/spectator/inst/doc")
 fic <- list.files(dirto)
