d5cafa8ed4333dba007517561627cfd57c33bb29
Zivan Karaman <30553159+zivankaraman@users.noreply.github.com>
2021-12-09 20:02:28 +0100

    fixed knit vignette

--- dev/knit-vignette.R
+++ dev/knit-vignette.R
@@ -4,10 +4,9 @@ rmarkdown::render(input = "C:/GitHub/spectator/vignettes2/UsingSpectator.Rmd", o
 file.rename("vignettes", "vignettes1")
 file.rename("vignettes2", "vignettes")
 # build with real vignette
-devtools::build()
+tgz <- devtools::build()
 # extract updated vignette files for doc
 chemin <- gsub("\\\\", "/", tempfile("dir"))
-tgz <- "C:/GitHub/spectator_0.1.0.tar.gz"
 untar(tgz, files = "spectator/inst/doc", exdir = chemin)
 dirfrom <- paste0(chemin, "/spectator/inst/doc")
 file.copy(from = list.files(dirfrom,full.names = TRUE), to = "./documentation/", overwrite = TRUE)
@@ -17,10 +16,23 @@ file.rename("vignettes1", "vignettes")
 # build with fake vignette
 devtools::build()
 # fix the tar file
-source("dev/fixtar.R")
+bkp <- sub("tar.gz", "bkp.tar.gz", tgz, fixed = TRUE)
+file.copy(from = tgz, to = bkp, overwrite = TRUE)
+chemin <- gsub("\\\\", "/", tempfile("dir"))
+untar(tgz, exdir = chemin)
+dirto <- paste0(chemin, "/spectator/inst/doc")
+fic <- list.files(dirto)
+dirfrom <- "./documentation/"
+file.copy(from = paste0(dirfrom, fic), to = dirto, overwrite = TRUE)
+oldwd <- getwd()
+setwd(chemin)
+tar(tarfile = tgz, files = "spectator", compression = "gzip")
+setwd(oldwd)
 # re-install
 detach("package:spectator", unload = TRUE)
 install.packages(tgz, repos = NULL, type = "source")
+# copy package archive to latest
+file.copy(from = tgz, to = "spectator_latest.tar.gz", overwrite = TRUE)
 
 # check on rhub
 chk <- rhub::check_for_cran(tgz, show_status = FALSE)
