b35dc9ec81bc71fd2bcaec1b0d471c05dcb28a68
Zivan Karaman <30553159+zivankaraman@users.noreply.github.com>
2024-07-16 18:52:59 +0200

    update autoload

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,6 @@
 .onLoad <- function(libname, pkgname) {
+    cat("Updating satellites database from Spectator Earth API ... ", file = stderr())
     satellites_db <- GetAllSatellites(positions = FALSE)
     assign("satellites_db", satellites_db, envir = parent.env(environment()))
+    cat("done.", sep = "\n", file = stderr())
 }
